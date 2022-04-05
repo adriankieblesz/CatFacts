8edc1f0dddfeb40e568843d759878c7e4e98d400
Adrian Kieblesz <adriano.kieblesz@gmail.com>
2022-04-05 17:03:11 +0200

    initial commit

---
+++ saveFile.js
@@ -0,0 +1,13 @@
+﻿function saveFile(fileName, content) {
+    var link = document.createElement('a');
+
+    link.download = fileName;
+
+    link.href = "data:text/plain; charset=utf-8," + encodeURIComponent(content);
+
+    document.body.appendChild(link);
+
+    link.click();
+
+    document.body.removeChild(link);
+}
